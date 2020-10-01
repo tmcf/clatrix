0d1425f29d6b8fd32b740f442368f7d098b5f92e
Ted McFadden <ted@errigal751.com>
2020-10-01 14:38:27 +1000

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clatrix/clatrix "0.5.1-SNAPSHOT"
+(defproject tmcf/clatrix "0.5.1-SNAPSHOT"
   :description "Because using matrices in Clojure needs to not suck."
   :url "http://tel.github.com/clatrix"
   :license {:name "MIT License"
